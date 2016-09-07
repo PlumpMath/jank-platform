886bccd050ac44b767f8748efc5f7c57ee80dc97
jeaye <contact@jeaye.com>
2016-09-07 09:34:48 -0700

    Refactor out some serial helpers

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 #include "multiboot.hpp"
-#include "asm.hpp"
+#include "serial.hpp"
 
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
@@ -11,31 +11,6 @@
 #if !defined(__i386__)
 #error "This OS needs to be compiled with a ix86-elf compiler"
 #endif
-
-#define PORT 0x3f8   /* COM1 */
-
-void init_serial()
-{
-   outb(PORT + 1, 0x00);    // Disable all interrupts
-   outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-   outb(PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
-   outb(PORT + 1, 0x00);    //                  (hi byte)
-   outb(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
-   outb(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
-   //outb(PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
-}
-
-int is_transmit_empty()
-{ return inb(PORT + 5) & 0x20; }
-
-void write_serial(char const * const s)
-{
-  while(is_transmit_empty() == 0)
-  { } /* Wait */
-
-  for(auto c(s); *c; ++c)
-  { outb(PORT, *c); }
-}
 
 char const* itoa(int val, int const base)
 {
---
+++ src/serial.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "asm.hpp"
+
+#define COM1 0x3f8
+
+void init_serial()
+{
+   outb(COM1 + 1, 0x00);    // Disable all interrupts
+   outb(COM1 + 3, 0x80);    // Enable DLAB (set baud rate divisor)
+   outb(COM1 + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
+   outb(COM1 + 1, 0x00);    //                  (hi byte)
+   outb(COM1 + 3, 0x03);    // 8 bits, no parity, one stop bit
+   outb(COM1 + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
+   //outb(COM1 + 4, 0x0B);    // IRQs enabled, RTS/DSR set
+}
+
+int is_transmit_empty()
+{ return inb(COM1 + 5) & 0x20; }
+
+void write_serial(char const * const s)
+{
+  while(is_transmit_empty() == 0)
+  { } /* Wait */
+
+  for(auto c(s); *c; ++c)
+  { outb(COM1, *c); }
+}
