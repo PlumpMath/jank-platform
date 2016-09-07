f3ca76b005a53a66e8dc4b8b0c3d254381177218
jeaye <contact@jeaye.com>
2016-09-07 09:27:30 -0700

    Write some helpful multiboot info to serial

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -37,6 +37,15 @@ void write_serial(char const * const s)
   { outb(PORT, *c); }
 }
 
+char const* itoa(int val, int const base)
+{
+  static char buf[32]{};
+  int i{ 30 };
+  for(; val && i; --i, val /= base)
+  { buf[i] = "0123456789abcdef"[val % base]; }
+  return &buf[i + 1];
+}
+
 void putpixel(unsigned char * const screen,
               size_t const where,
               int32_t const color)
@@ -46,30 +55,46 @@ void putpixel(unsigned char * const screen,
     screen[where + 2] = (color >> 16) & 255; // R
 }
 
-extern "C" void kernel_main(int32_t const ebx, int32_t const )
+extern "C" void kernel_main(multiboot_info const * const multiboot)
 {
   init_serial();
-  write_serial("meow!\n");
+  write_serial("serial initialized\n");
 
-  if(!ebx)
-  { return; } // TODO: Error
+  if(!multiboot)
+  {
+    write_serial("multiboot is invalid\n");
+    return;
+  }
 
-  auto const &multiboot(*reinterpret_cast<multiboot_info*>(ebx));
+  auto const &mboot(*multiboot);
 
-  auto const width(multiboot.framebuffer_width);
-  auto const height(multiboot.framebuffer_height);
+  auto const width(mboot.framebuffer_width);
+  write_serial("framebuffer: ");
+  write_serial(itoa(width, 10));
+
+  auto const height(mboot.framebuffer_height);
+  write_serial("x");
+  write_serial(itoa(height, 10));
+
+  uint32_t const bpp{ mboot.framebuffer_bpp };
+  write_serial(" bpp: ");
+  write_serial(itoa(bpp, 10));
+
+  uint32_t const pitch{ mboot.framebuffer_pitch };
+  write_serial(" pitch: ");
+  write_serial(itoa(pitch, 10));
+  write_serial("\n");
+
   for(size_t y{}; y < height; ++y)
   {
     for(size_t x{}; x < width; ++x)
     {
-      uint32_t const pitch{ multiboot.framebuffer_pitch };
-      uint32_t const bpp{ multiboot.framebuffer_bpp };
       uint32_t const pixel_width{ bpp / 8 };
-      uint64_t const framebuffer{ multiboot.framebuffer_addr };
+      uint64_t const framebuffer{ mboot.framebuffer_addr };
       uint64_t pixel_offset = (y * pitch) + (x * pixel_width);
       putpixel(reinterpret_cast<unsigned char*>(framebuffer),
                pixel_offset,
-               0x000000);
+               0xFF0000);
     }
   }
 }
